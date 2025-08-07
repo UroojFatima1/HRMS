acbc1480248facf324dc1338369413b68a9e135b
Urooj Fatima <uroojfatima1518@gmail.com>
2025-08-07 07:11:26 -0400

    Delete api update

--- src/app/api/manageemployee/[id]/route.js
+++ src/app/api/manageemployee/[id]/route.js
@@ -1,51 +1,49 @@
 import { NextResponse } from "next/server";
 import Employee from "@/model/User";
+import Attendance from "@/model/Attendance";
+import Payroll from "@/model/Payroll";
 import { connectDB } from "@/lib/dbconnect";
 
-export async function DELETE(req, { params }) 
-{
+export async function DELETE(req, { params }) {
+   
     const { id } = await params;
     await connectDB();
 
-    try
-    {
+    try {
+
         const deletedEmp = await Employee.findByIdAndDelete(id);
-        if (!deletedEmp)
-        {
+        if (!deletedEmp) {
             return NextResponse.json({ error: "Employee not found" }, { status: 404 });
         }
+        await Attendance.deleteMany({ userId: id });
 
-        return NextResponse.json({ message: "Employee deleted" });
-    } catch (err)
-    {
+        await Payroll.deleteMany({ userId: id });
+
+        return NextResponse.json({ message: "Employee and related data deleted" });
+    } catch (err) {
         return NextResponse.json({ error: "Server error" }, { status: 500 });
     }
 }
 
-
-export async function PUT(req, { params })
-{
+export async function PUT(req, { params }) {
     await connectDB();
+    
     const { id } = await params;
     const { name, email, position, salary } = await req.json();
 
-    try
-    {
+    try {
         const updatedEmp = await Employee.findByIdAndUpdate(
             id,
             { name, email, position, salary },
             { new: true }
         );
 
-        if (!updatedEmp)
-        {
+        if (!updatedEmp) {
             return NextResponse.json({ error: "Employee not found" }, { status: 404 });
         }
 
         return NextResponse.json({ message: "Employee updated", user: updatedEmp });
-    }
-    catch (err)
-    {
+    } catch (err) {
         return NextResponse.json({ error: "Update failed" }, { status: 500 });
     }
 }
